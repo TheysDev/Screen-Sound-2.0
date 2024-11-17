d399f827215a83ef017699e419b9e9ec8f6efa56
Matheus Borges <theys.dev@gmail.com>
2024-11-16 22:24:10 -0300

    Adicionado Opção de Avaliar Álbuns

--- Program.cs
+++ Program.cs
@@ -12,6 +12,13 @@ bandasRegistradas.Add(ira.Nome, ira);
 bandasRegistradas.Add(beatles.Nome, beatles);
 
 Dictionary<int, Menu> menus= new();
+menus.Add(1, new MenuRegistrarBanda());
+menus.Add(2, new MenuRegistrarAlbum());
+menus.Add(3, new MenuMostrarBandas());
+menus.Add(4, new MenuAvaliarBanda());
+menus.Add(5, new MenuAvaliarAlbum());
+menus.Add(6, new MenuExibirDetalhes());
+menus.Add(-1, new MenuSair());
 
 void ExibirLogo()
 {
@@ -34,19 +41,23 @@ void ExibirOpcoesDoMenu()
     Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
     Console.WriteLine("Digite 3 para mostrar todas as bandas");
     Console.WriteLine("Digite 4 para avaliar uma banda");
-    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+    Console.WriteLine("Digite 5 para avaliar um álbum");
+    Console.WriteLine("Digite 6 para exibir os detalhes de uma banda");
     Console.WriteLine("Digite -1 para sair");
 
     Console.Write("\nDigite a sua opção: ");
     string opcaoEscolhida = Console.ReadLine()!;
     int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
-    if(menus.ContainsKey(opcaoEscolhidaNumerica))
+    
+    if (menus.ContainsKey(opcaoEscolhidaNumerica))
     {
-        menus[opcaoEscolhidaNumerica].Executar(bandasRegistradas);
-    }else
+        Menu menuASerExibido = menus[opcaoEscolhidaNumerica];
+        menuASerExibido.Executar(bandasRegistradas);
+        if (opcaoEscolhidaNumerica > 0) ExibirOpcoesDoMenu();
+    } 
+    else
     {
         Console.WriteLine("Opção inválida");
     }
 }
-
 ExibirOpcoesDoMenu();
